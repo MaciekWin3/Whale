2a21c911a11bf0fae7b761c9425769dd7cfdae33
maciekwin3 <maciekwin3@gmail.com>
2023-02-18 19:43:50 +0100

    Add project files.

---
+++ Whale.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Whale.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Whale/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+RunProcess("docker ps -a");
+
+static void RunProcess(string command)
+{
+    Process process = new();
+    process.StartInfo.FileName = "pwsh.exe";
+    process.StartInfo.Arguments = $"/c {command}";
+    process.StartInfo.UseShellExecute = false;
+    process.StartInfo.RedirectStandardOutput = true;
+    process.Start();
+
+    string output = process.StandardOutput.ReadToEnd();
+
+    process.WaitForExit();
+    Console.WriteLine(output);
+}
