f70d5a68eb10625b529aafe1555357b607f0f98e
Maciej Winnik <maciekwin3@gmail.com>
2023-07-02 15:52:28 +0200

    Update menu bar

--- Whale/Components/MenuBarX.cs
+++ Whale/Components/MenuBarX.cs
@@ -1,4 +1,6 @@
-﻿using Terminal.Gui;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Terminal.Gui;
 
 namespace Whale.Components
 {
@@ -8,30 +10,68 @@ namespace Whale.Components
         {
             var menuBar = new MenuBar(new MenuBarItem[]
             {
-                new MenuBarItem("Whale", CreateSomething())
+                new MenuBarItem("Whale", CreateMainMenuBar()),
+                new MenuBarItem("Help", CreateMenuItems()),
             });
 
             return menuBar;
         }
 
-        private static List<MenuItem[]> CreateSomething()
+        private static List<MenuItem[]> CreateMainMenuBar()
         {
             var menuItems = new List<MenuItem[]>();
             var fileMenu = new MenuItem[]
             {
                 new MenuItem("Open", "", () => MessageBox.Query("Hello", "This is something")),
+                new MenuItem("Quit", "", () => Application.RequestStop()),
             };
             menuItems.Add(fileMenu);
-            menuItems.Add(CreateMenuItems());
             return menuItems;
         }
 
-        private static MenuItem[] CreateMenuItems()
+        private static List<MenuItem[]> CreateMenuItems()
         {
-            List<MenuItem> menuItems = new();
-            var quit = new MenuItem("Quit", "", () => Application.RequestStop());
-            menuItems.Add(quit);
-            return menuItems.ToArray();
+            var menuItems = new List<MenuItem[]>();
+            var fileMenu = new MenuItem[]
+            {
+                new MenuItem("About", "", () => MessageBox.Query("Hello", "This is something")),
+                new MenuItem("Readme.md", "", () => OpenUrl("https://github.com/MaciekWin3/Whale"))
+            };
+            menuItems.Add(fileMenu);
+            return menuItems;
+
+        }
+
+        static void OpenUrl(string url)
+        {
+            try
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = "xdg-open",
+                        Arguments = url,
+                        RedirectStandardError = true,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true,
+                        UseShellExecute = false
+                    });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
     }
 }
