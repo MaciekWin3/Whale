1bc398cbcc14e724a9b4d25950ca6aeb982c683f
Maciej Winnik <maciekwin3@gmail.com>
2023-07-10 10:37:49 +0200

    Result tests

---
+++ Whale.Tests/Utils/Extensions/ResultExtensionTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Whale.Utils;
+using Whale.Utils.Extensions;
+
+namespace Whale.Tests.Utils.Extensions
+{
+    [TestFixture]
+    public class ResultExtensionTests
+    {
+        [Test]
+        public void ShouldThrowInvalidOperationExceptionForMapFailIfResultIsNull()
+        {
+            // Arrange
+            Result result = null!;
+
+            // Act
+            Action toOkResult = () => result.MapFail<string>();
+
+            // Assert
+            toOkResult.Should().Throw<InvalidOperationException>();
+        }
+
+        [Test]
+        public void ShouldReturnFailResultForMapFailIfResultIsNotSuccessful()
+        {
+            // Arrange
+            var result = Result.Fail<string>("Error message example");
+            var expectedFailResult = Result.Fail<string>(result.Error!);
+
+            // Act
+            var failResult = result.MapFail<string>();
+
+            // Assert
+            failResult.Should().BeEquivalentTo(expectedFailResult);
+        }
+    }
+}
---
+++ Whale.Tests/Utils/ResultTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Whale.Utils;
+
+namespace Whale.Tests.Utils
+{
+    [TestFixture]
+    public class ResultTests
+    {
+        private object testObject;
+        private const string errorMessageExample = "Example error message";
+
+        [SetUp]
+        public void Setup()
+        {
+            testObject = new object();
+        }
+
+        [Test]
+        public void ShouldReturnFailResult()
+        {
+            var result = Result.Fail(errorMessageExample);
+
+            result.Error.Should().Be(errorMessageExample);
+            result.IsSuccess.Should().BeFalse();
+            result.IsFailure.Should().BeTrue();
+        }
+
+        [Test]
+        public void ShouldReturnOkResult()
+        {
+            var result = Result.Ok();
+
+            result.Error.Should().Be(string.Empty);
+            result.IsSuccess.Should().BeTrue();
+            result.IsFailure.Should().BeFalse();
+        }
+
+        [Test]
+        public void ShouldReturnOkResultForGeneric()
+        {
+            var result = Result.Ok(testObject);
+            result.Error.Should().Be(string.Empty);
+            result.Value.Should().BeEquivalentTo(testObject);
+            result.IsSuccess.Should().BeTrue();
+            result.IsFailure.Should().BeFalse();
+        }
+    }
+}
